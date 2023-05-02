04022e6cfce73d30d2ce06aead38443b0833f663
vixbc <92274245+vixbc@users.noreply.github.com>
2023-05-02 16:12:54 +0100

    completed tutorial build

--- react-notes-app/src/components/Note.js
+++ react-notes-app/src/components/Note.js
@@ -1,16 +1,19 @@
-import {MdDeleteForever} from 'react-icons/md';
+import { MdDeleteForever } from 'react-icons/md';
 
-const Note = () => {
-    return (
-    <div className="note">
-        <span>hello. this is our first note.</span>
-        <div className="note-footer">
-            <small>13/04</small>
-            <MdDeleteForever className='delete-icon' size='1.3em'/>
-        </div>
-    </div>
-    );
-    
+const Note = ({ id, text, date, handleDeleteNote }) => {
+	return (
+		<div className='note'>
+			<span>{text}</span>
+			<div className='note-footer'>
+				<small>{date}</small>
+				<MdDeleteForever
+					onClick={() => handleDeleteNote(id)}
+					className='delete-icon'
+					size='1.3em'
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default Note;
