af5edfef0b6bf2ceb1fd9ac0d7537c67595ea218
Ying Huai <yinghuai@yingdembp.home>
2023-05-03 22:35:23 +0100

    change background-image from image to linear-gradient color

---
+++ react-notes-app/src/Components/NoteComponents/CreateNote.js
@@ -0,0 +1,34 @@
+import React from "react";
+import LinearProgress from "@mui/material/LinearProgress";
+function CreateNote({ textHandler, saveHandler, inputText }) {
+  //character limit is 140
+  const charLimit = 140;
+  const charLeft = charLimit - inputText.length;
+  //Progress bar percentage
+  const props = ( charLimit - inputText.length) / charLimit * 100;
+  return (
+    <div className="note" style={{ background: "none"}}>
+      <textarea
+        cols="10"
+        rows="5"
+        value={inputText}
+        placeholder="What's happening?"
+        onChange={textHandler}
+        maxLength="140"
+      ></textarea>
+      <div className="note__footer">
+        <span className="label"> You have {charLeft} left</span>
+        <button className="note__save" onClick={saveHandler}>
+          Save
+        </button>
+      </div>
+      <LinearProgress
+      className="char__progress"
+      variant="determinate"
+      value={props}
+      />
+    </div>
+  );
+}
+
+export default CreateNote;
